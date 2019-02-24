038d5dc020920dc74155c0a050c8db2dcaa36274
juhandi14 <46861774+juhandi14@users.noreply.github.com>
2019-02-24 08:29:43 +0000

    Delete table_mobil.sql

--- table_mobil.sql
+++
@@ -1,8 +0,0 @@
-create table mobil (
-kode varchar(15) not null,
-jenis varchar(15) not null,
-merk varchar(20) not null,
-tarif varchar(20) not null,
-nopol varchar(20) not null,
-primary key(kode)
-);
